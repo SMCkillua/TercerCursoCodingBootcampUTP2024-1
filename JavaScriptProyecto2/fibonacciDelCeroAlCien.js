65be3e86cd5f93c4cda74744ccb7d54b39966523
SMCkillua <147349869+SMCkillua@users.noreply.github.com>
2024-04-19 11:07:40 -0500

    Update fibonacciDelCeroAlCien.js

--- JavaScriptProyecto2/fibonacciDelCeroAlCien.js
+++ JavaScriptProyecto2/fibonacciDelCeroAlCien.js
@@ -1,15 +1,1 @@
-imprimirFibonacci()
-function imprimirFibonacci() {
-    let i = 0
-    while (i <= 20) {
-        console.log(calcularFibonacci(i))
-        i++
-    }
-}
-function calcularFibonacci(numero) {
-    if (numero <= 1) {
-        return numero
-    } else {
-        return calcularFibonacci(numero - 1) + calcularFibonacci(numero - 2)
-    }
-}
+
