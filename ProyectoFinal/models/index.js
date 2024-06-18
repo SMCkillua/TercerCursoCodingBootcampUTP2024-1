11e4883807fb07d942a54368cba54ca4723f13e4
SMCkillua~ <147349869+SMCkillua@users.noreply.github.com>
2024-06-17 22:15:38 -0500

    Revert "Todo mal"
    This reverts commit 7e8e731b43422959e34d5cb26b600720675a2927.

--- ProyectoFinal/models/index.js
+++
@@ -1,9 +0,0 @@
-import { sequelize } from "../config/config.js";
-
-
-const syncModels = async () => {
-  await sequelize.sync({ force: false });
-  console.log("Los modelos se vincularon correctamente");
-};
-
-export { syncModels };
